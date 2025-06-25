5e898971dc40c9a483831dbd3169023c8805b870
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-06-25 23:58:57 +0300

    KVKK ve Çerez Politikası yaptım

---
+++ public/kvkk.php
@@ -0,0 +1,74 @@
+<?php require_once 'init.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>İletişim - MatReklam</title>
+    <?php include 'include/head.html'; ?>
+</head>
+
+<body>
+    <?php include 'include/header.html'; ?>
+
+    <main class="sub-page">
+        <section class="hero-section referances-page">
+            <div class="container-fluid">
+                <div class="row col-lg-10 offset-lg-1 justify-content-center align-items-center mb-5">
+                    <h1 class="text-center">(K.V.K.K) Kişisel Verileri Koruma Kanunu</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="referances-section referances-area">
+            <div class="container-fluid">
+                <div class="row justify-content-center scroll-reveal-top">
+                    <div class="col-lg-10 text-white">
+                       
+                        <h2>Kişisel Verilerin Korunması Politikamız</h2>
+                        <p>MatReklam İnşaat olarak, kişisel verilerinizin güvenliği ve gizliliği konusunda büyük hassasiyet göstermekteyiz. 6698 sayılı Kişisel Verilerin Korunması Kanunu ("KVKK") kapsamında, kişisel verilerinizin işlenmesi, saklanması ve korunması konusunda aşağıdaki politikayı uygulamaktayız.</p>
+
+                        <h3>Kişisel Verilerin İşlenme Amacı</h3>
+                        <p>Kişisel verileriniz aşağıdaki amaçlar doğrultusunda işlenmektedir: </p>
+
+                        <ul>
+                            <li>İş başvurularınızın değerlendirilmesi ve işe alım süreçlerinin yürütülmesi</li>
+                            <li>İş ilişkisi kurulması ve yürütülmesi</li>
+                            <li>Yasal yükümlülüklerimizin yerine getirilmesi</li>
+                            <li>İş sağlığı ve güvenliği faaliyetlerinin yürütülmesi</li>
+                            <li>Şirket içi iletişim ve organizasyon faaliyetlerinin yürütülmesi</li>
+                            <li>Müşteri ilişkileri yönetimi ve hizmet kalitesinin artırılması</li>
+                            <li>
+                                Kişisel Verilerin Aktarılması: Kişisel verileriniz, yasal yükümlülüklerimiz kapsamında yetkili kamu kurum ve kuruluşlarına, iş ortaklarımıza ve hizmet sağlayıcılarımıza aktarılabilecektir. Bu aktarımlar, KVKK'nın 8. ve 9. maddelerinde belirtilen şartlar ve amaçlarla sınırlı olarak gerçekleştirilecektir.
+                            </li>
+                        </ul>
+
+                        <h4>Kişisel Verilerin Korunması</h4>
+                        <p>Kişisel Veri Sahibinin Hakları
+                        KVKK'nın 11. maddesi uyarınca, kişisel veri sahibi olarak aşağıdaki haklara sahipsiniz:</p>
+
+                        <ul>
+                            <li>Kişisel verilerinizin işlenip işlenmediğini öğrenme</li>
+                            <li>Kişisel verileriniz işlenmişse buna ilişkin bilgi talep etme</li>
+                            <li>Kişisel verilerinizin işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme</li>
+                            <li>Yurt içinde veya yurt dışında kişisel verilerinizin aktarıldığı üçüncü kişileri bilme</li>
+                            <li>Kişisel verilerinizin eksik veya yanlış işlenmiş olması hâlinde bunların düzeltilmesini isteme</li>
+                            <li>KVKK'nın 7. maddesinde öngörülen şartlar çerçevesinde kişisel verilerinizin silinmesini veya yok edilmesini isteme</li>
+                            <li>Kişisel verilerinizin aktarıldığı üçüncü kişilere yukarıda belirtilen düzeltme, silme veya yok etme işlemlerinin bildirilmesini isteme</li>
+                            <li>İşlenen verilerinizin münhasıran otomatik sistemler vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya çıkmasına itiraz etme</li>
+                            <li>Kişisel verilerinizin kanuna aykırı olarak işlenmesi sebebiyle zarara uğramanız hâlinde zararın giderilmesini talep etme</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include 'include/footer.html'; ?>
+
+</body>
+
+</html>
